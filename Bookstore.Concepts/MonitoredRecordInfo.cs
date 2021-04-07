28e4a83f4553d4909bc64f2f0535da158fa76461
mcapan <mcapan@omega-software.hr>
2021-04-07 12:56:59 +0200

    Added MonitoredRecordInfo

--- Bookstore.Concepts/MonitoredRecordInfo.cs
+++ Bookstore.Concepts/MonitoredRecordInfo.cs
@@ -9,7 +9,35 @@ using Rhetos.Dsl.DefaultConcepts;
 
 namespace Bookstore.Concepts
 {
-    //[Export(typeof(IConceptInfo))]
-    //[ConceptKeyword("MonitoredRecord")]
+    /// <summary>
+    /// When user tries to deletes the records, it will automatically modify the operation deactivate those records instead
+    /// (set the Active property value to false).
+    /// Automatic deactivation is only applied for user requests.
+    /// It is not applied if the Delete method is explicitly called from code, as a part of some other operation.
+    /// </summary>
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("MonitoredRecord")]
+    public class MonitoredRecordInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public EntityInfo Entity { get; set; }
+    }
 
+    [Export(typeof(IConceptMacro))]
+    public class MonitoredRecordMacro : IConceptMacro<MonitoredRecordInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(MonitoredRecordInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            newConcepts.Add(new EntityLoggingInfo { Entity = conceptInfo.Entity });
+
+            var createdAt = new DateTimePropertyInfo { DataStructure = conceptInfo.Entity, Name = "CreatedAt" };
+            newConcepts.Add(createdAt);
+            newConcepts.Add(new CreationTimeInfo { Property = createdAt });
+
+
+            return newConcepts;
+        }
+    }
 }
