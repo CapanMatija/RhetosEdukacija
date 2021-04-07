2dfe567cd074a9f6c10a92bc4646d8c11ed945aa
mcapan <mcapan@omega-software.hr>
2021-04-07 10:07:44 +0200

    Reorganised structure

---
+++ BookStore.Service/src/Bookstore.Concepts/DeactivateOnDeleteInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Web;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+using Rhetos.Compiler;
+using Rhetos.Extensibility;
+using Rhetos.Dom.DefaultConcepts;
+
+namespace BookStore.Service.src.Bookstore.Concepts
+{
+    [Export (typeof(IConceptInfo))]
+    [ConceptKeyword("DeactivateOnDelete")]
+    public class DeactivateOnDeleteInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public DeactivatableInfo Deactivatable { get; set; }
+    }
+
+    [Export(typeof(IConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(DeactivateOnDeleteInfo))]
+    public class DeactivateOnDeleteCodeGenerator : IConceptCodeGenerator
+    {
+        public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (DeactivateOnDeleteInfo)conceptInfo;
+
+            var code = String.Format(
+                @"var deactivated = deleted.ToList();
+
+                foreach(var item in deleted)
+                    item.Active = false;
+
+                updated = updated.Concat(deleted).ToArray();
+                updatedNew = updatedNew.Concat(deleted).ToArray();
+
+                deleted = new Common.Queryable.{0}_{1}[]{{}};
+                deleted = new {0}.{1}[]{{}};
+                ",
+                    info.Deactivatable.Entity.Module.Name,
+                    info.Deactivatable.Entity.Name);
+
+            codeBuilder.InsertCode(code, WritableOrmDataStructureCodeGenerator.OldDataLoadedTag, info.Deactivatable.Entity);
+        }
+    }
+}
